37620cd86cda229b0eb400e457891212a351758d
Graham Greenfield <grahamg@gmail.com>
2023-09-03 00:47:16 -0500

    startup message

--- server.js
+++ server.js
@@ -61,5 +61,5 @@ app.get('/search', async (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}, aloha! 🌺`);
+    console.log(`Server is running on port ${PORT}...`);
 });
